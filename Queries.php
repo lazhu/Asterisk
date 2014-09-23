6e7aef59a5531a6aeb71027a8ab07c4c6fa7bd64
Lazhu <lazhu.gonish@gmail.com>
2014-09-23 18:18:13 +0400

    More classes

---
+++ Queries.php
@@ -0,0 +1,12 @@
+<?php
+abstract class Queries {
+
+	protected $sql;
+	protected $data;
+
+	public function __construct($options){
+		$this->sql = $options['sql'];
+		$this->data = $options['data'];
+	}
+}
+?>
---
+++ Trunks.php
@@ -0,0 +1,15 @@
+<?php
+require_once('Queries.php');
+class Trunks extends Queries{
+
+	public function getTrunkByProvider(){
+		$query = "SELECT trunk FROM trunks WHERE provider = ?";
+		$result = $this->sql->select(array(PDO::FETCH_NUM), 'fetch', $query, array($this->data));
+		return $result[0];
+	}
+
+	public function __construct($options){
+		parent::__construct($options);
+	}
+}
+?>
