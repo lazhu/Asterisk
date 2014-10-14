29d2bee70512705d95a90ccd7665252224a6324f
Lazhu <lazhu.gonish@gmail.com>
2014-10-14 15:33:33 +0400

    Hangup exception

---
+++ src/Asterisk/Agi/Pinned.php
@@ -0,0 +1,17 @@
+<?php
+namespace Asterisk\Agi;
+use Asterisk\Sql\Persons;
+class Pinned extends External {
+
+	public function __construct($options){
+		preg_match('/^[0-9]*#/', $options['dnis'], $pin);
+		$persons = new Persons(array(
+			'sql' => $options['sql']
+		));
+		if(!$persons->checkPin($pin[0])){
+			throw new \Asterisk\Hangup;
+		}
+		$options['dnis'] = substr($options['dnis'], strlen($pin[0]));
+		parent::__construct($options);
+	}
+}
---
+++ src/Asterisk/Hangup.php
@@ -0,0 +1,4 @@
+<?php
+namespace Asterisk;
+class Hangup extends \Exception {
+}
